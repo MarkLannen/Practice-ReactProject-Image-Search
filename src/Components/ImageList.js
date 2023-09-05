6b609b99009576f5c5f78b00c9df068542187744
Mark.Lannen <mark.lannen@mso.umt.edu>
2023-09-05 10:24:31 -0600

    bring in images from API and display from ImageShow.js component

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -2,7 +2,7 @@ import React from "react";
 import imageShow from "./ImageShow";
 import ImageShow from "./ImageShow";
 const ImageList = ({ images }) => {
-  const renderedImages = images.map((image => {
+  const renderedImages = images.map((image) => {
     return <ImageShow key={image.id} image={image} />;
   });
   return <div>{renderedImages}</div>;
--- src/Components/ImageShow.js
+++ src/Components/ImageShow.js
@@ -2,7 +2,7 @@ import React from "react";
 const ImageShow = ({ image }) => {
   return (
     <div>
-      {image.id}, {image.alt_description}
+      <img src={image.urls.small} alt={image.alt_desription} />
     </div>
   );
 };
